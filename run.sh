67c02516948a059c29acc2ee23b414395490488d
s4ndwitch <sandwitch@postman.i2p>
2024-02-27 07:13:02 +0300

    f finally

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-as --32 -o $1.o $1.S
-ld --oformat binary -Ttext 0x7c00 -o $1.bin -m elf_i386 $1.o
-qemu-system-i386 $1.bin
-rm $1.o $1.bin
+as --32 kernel.S -o kernel.o && as --32 bootloader.S -o bootloader.o
+ld --oformat binary -Ttext 0x7c00 -o bootloader.bin -m elf_i386 bootloader.o && ld --oformat binary -Ttext 0x1000 -o kernel.bin -m elf_i386 kernel.o
+cat bootloader.bin kernel.bin > os-image
+qemu-system-i386 os-image
+rm bootloader.o bootloader.bin kernel.o kernel.bin
